5e069b27bda4960eb3680fe9e8d435dadbaef635
Altus Infotech <Altusinfotech.it@gmail.com>
2021-07-10 16:43:02 +0530

    End of section 7

---
+++ API/Errors/APIException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Errors
+{
+    public class APIException
+    {
+        public APIException(int StatusCode, string Message = null, string Details = null)
+        {
+            this.StatusCode = StatusCode;
+            this.Message = Message;
+            this.Details = Details;
+        }
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string Details { get; set; }
+    }
+}
