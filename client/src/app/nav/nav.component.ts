e520babb785764137eb3c376862869d14368e934
Altus Infotech <Altusinfotech.it@gmail.com>
2021-07-10 12:22:24 +0530

    End of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,7 +14,7 @@ export class NavComponent implements OnInit {
 
   model: any = {}
 
-  constructor(public accountService: AccountService) {
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) {
   }
 
   ngOnInit(): void {
@@ -21,24 +23,22 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
-      // this.loggedIn = true;
+      this.router.navigateByUrl("/members");
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
   logout() {
     this.accountService.logout();
-    // this.loggedIn = false;
+    this.router.navigateByUrl("/");
   }
 
-  //getCurrentUser() {
-  //  this.accountService.currentUser$.subscribe(user => {
-  //    debugger;
-  //    this.loggedIn = user == null || user.userName == undefined || user.userName == '' ? false : true;
-  //  }, error => {
+  getUsername() {
+    let currentUser = JSON.parse(localStorage.getItem('user') || '{}');
+    let username = currentUser.userName.charAt(0).toUpperCase() + currentUser.userName.slice(1);
+    return username;
+  }
 
-  //  })
-  //}
 }
